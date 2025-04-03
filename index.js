651add90081eb3e9d9d0813808c913a2a2ee68fd
lucasxnarezzi <lucasxnarezzi@gmail.com>
2025-04-03 17:33:05 -0300

    concluindo sistema

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 rodada = 1
 alert("Seja mal-vindo a Roleta Russa!")
+
 while (rodada <= 6) {
     escolhaJogador = prompt("Rodada " + rodada +" - Escolha entre 1-6")
+    opcoes = ["1", "2", "3", "4", "5", "6"]
+    
+    if (!opcoes.includes(escolhaJogador)){
+        alert("buraco errado!")
+        continue;
+    }
+    
+    if (rodada === 6){
+        tiro = escolhaJogador
+        alert("Morreu otário! A bala estava no " + escolhaJogador)
+        break;
+    }
+
     tiro = Math.floor(Math.random() * 6) + 1
+    
     if (escolhaJogador == tiro) {
         alert("Você morreu!🤯🩸🔫")
         break;
     } else {
         alert("Você sobreviveu, a bala estava no " + tiro)
     }
-    rodada ++
+    rodada++
 }
